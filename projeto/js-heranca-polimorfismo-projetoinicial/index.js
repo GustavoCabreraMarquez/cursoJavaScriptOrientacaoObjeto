104f5ad6624237cc7339ca32c64e8d6793273edd
Gustavo Cabrera <gugis360@gmail.com>
2022-03-23 10:08:33 -0300

    operacoes de conta agora padronizadas em modulo

--- projeto/js-heranca-polimorfismo-projetoinicial/index.js
+++ projeto/js-heranca-polimorfismo-projetoinicial/index.js
@@ -1,15 +1,17 @@
 import {Cliente} from "./Cliente.js"
+import { Conta } from "./conta.js";
 import {ContaCorrente} from "./ContaCorrente.js"
 import {ContaPoupanca } from "./contaPoupanca.js";
 
+
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+const contaPoupanca = new Conta(50, cliente1, 1001)
 
 console.log(contaPoupanca)
 console.log(contaCorrenteRicardo)
-// console.log(contaCorrenteRicardo);
+// console.log(contaCorrenteRicardo);   
